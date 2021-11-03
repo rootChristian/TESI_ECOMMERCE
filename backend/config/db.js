7559d86d41de450edbb5fa17a6f31811c0139e20
rootChristian <63016296+rootChristian@users.noreply.github.com>
2021-11-03 12:26:08 +0100

    Update configuration db...

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,26 +3,23 @@
 ************ Version:    1.0.0                      ********************
 ***********************************************************************/
 
+/***********************************************************************
+************ Author:    Christian KEMGANG NGUESSOP ********************
+************ Version:    1.0.0                      ********************
+***********************************************************************/
+
 const mongoose = require('mongoose');
 require('dotenv').config({path: './config/.env'});
-/*
-mongoose
-    .connect('mongodb+srv://unipr:'+process.env.DB_USER_PASS+'@unipr.g2qmr.mongodb.net/unipr_db',
-    {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useCreateIndex: true,
-        useFindAndModify: false
-    }
-    )
-    .then(() => console.log('Connect to MongoDB...'))
-    .catch((err) => console.log('failed to connect to MongoDB...', err));
-*/
 
-mongoose.connect(
-    'mongodb+srv://unipr:' + process.env.DB_USER_PASS + '@unipr.g2qmr.mongodb.net/unipr_db',
-    async (err) => {
-        if (err) console.log('failed to connect to MongoDB...', err);
-        console.log("Connect to MongoDB...")
-    }
-)
+
+//Database connection
+const mongodbURL = 'mongodb+srv://'+process.env.DB_USER+':'+process.env.DB_USER_PASS+'@unipr.g2qmr.mongodb.net/'+process.env.DATABASE+'?retryWrites=true&w=majority'
+
+mongoose.connect(mongodbURL, { useNewUrlParser: true, useFindAndModify: false, useCreateIndex: true,  useUnifiedTopology: true })
+        .then(()=> {
+            console.log('We are using database: ' +process.env.DATABASE);
+            console.log('Database connection is ready...');
+        })
+        .catch((err)=>{
+            console.log('Database connection is failled...\n', err);
+        })
